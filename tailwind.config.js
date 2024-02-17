1046a62c7311d3aa4008bfe70a058cf359f2e4ee
irfan-s-m <know.cse@gmail.com>
2024-02-18 02:43:04 +0600

    main section ongoing

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,6 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {
-        width: {
-          '200': '70rem'
-      },
-        height: {
-          '550': '34rem'
-        }
     },
     plugins: [],
   }
